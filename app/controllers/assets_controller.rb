276d9c83ba0e61c3036611a83ea14f1d4b6e71bc
Russ Smith <russ@bashme.org>
2010-12-02 13:47:46 -0800

    Cleaning up model and controller files.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,7 +1,13 @@
 class AssetsController < ApplicationController
+  before_filter :find_asset, :except => [ :index, :new, :create ]
+
   def index
-    #search Asset.search { fulltext 'exhaust'}.results
+    # search Asset.search { fulltext 'exhaust'}.results
     @assets = Asset.all
+  end
+
+  def show
+    @asset = Asset.find(params[:id])
   end
 
   def new
@@ -12,29 +18,32 @@ class AssetsController < ApplicationController
     @asset = Asset.new(params[:asset])
     @asset.tag_list = params[:tags]
     if @asset.save
-      flash[:notice] = "Asset creation successful"
-      redirect_to @asset
+      redirect_to(@asset, :notice => "Asset creation successful")
     else
-      render :action => 'new'
+      render(:action => "new")
     end
   end
 
   def edit
-    @asset = Asset.find(params[:id])
   end
-  
+
   def update
-    @asset = Asset.find(params[:id])
     @asset.tag_list = params[:tags]
     if @asset.update_attributes(params[:asset])
-      flash[:notice] = "Asset successfully updated"
-      redirect_to @asset
+      redirect_to(@asset, :notice => "Asset successfully updated")
     else
-      render :action => 'edit'
+      render(:action => "edit")
     end
   end
 
-  def show
+  def destroy
+    @asset.destroy
+    redirect_to(assets_path, :notice => "Asset successfully deleted")
+  end
+
+private
+
+  def find_asset
     @asset = Asset.find(params[:id])
   end
 end
