ab6e363bc73d94c5c3708ed824b3d305cecfad55
Russ Smith <russ@bashme.org>
2010-12-02 13:17:07 -0800

    I'm a spacing nazi.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,9 +2,10 @@ class Asset < ActiveRecord::Base
   acts_as_taggable
 
   mount_uploader :file, AssetUploader
+
   searchable do
-    text  :name, :owner
-    text  :tags do
+    text :name, :owner
+    text :tags do
       tags.map { |tag| tag.name }
     end
   end
