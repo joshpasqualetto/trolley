31bc307b1d9acf4275a42a3a5fbc8cd782bdd14b
Russ Smith <russ@bashme.org>
2010-12-08 14:00:34 -0800

    Adding rollover effect.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $(".asset-summary").hover(
+    function() {
+      $(this).css("-webkit-transform", "scale(1.05)");
+      $(this).css("-moz-transform", "scale(1.05)");
+    },
+
+    function() {
+      $(this).css("-webkit-transform", "scale(1)");
+      $(this).css("-moz-transform", "scale(1)");
+    }
+  );
+});
